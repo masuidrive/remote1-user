9d2c74987b7cf3582539416f6a74ab403cce45ca
Yuichiro MASUI <masui@masuidrive.jp>
2016-11-24 23:39:24 +0900

    WIP

--- app/assets/javascripts/controllers/welcome_index_controller.jsx
+++ app/assets/javascripts/controllers/welcome_index_controller.jsx
@@ -9,9 +9,21 @@ class WelcomeIndexController extends ActionController {
   }
 
   renderIndex() {
+    console.log(this.props)
+    console.log(this.props.categories)
     return (
       <div>
         Welcome to Resume-1.
+        { this.props.categories.map((category, cidx) =>
+          <div key={category.uid}>
+            { category.label_ja } / { category.label_en }<br/>
+            { category.topics.map((topic, tidx) =>
+              <div key={topic.uid}>
+                { topic.body }
+              </div>
+            )}
+          </div>          
+        ) }
       </div>
     )
   }
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,25 +1,19 @@
 module ApplicationHelper
+
+  # react_props(
+  #   essays: {},
+  #   categories: { serializer: PublicCategorySerializer }
+  # )
   def react_props(*args)
-    name = nil
-    options = Hash[*
-      args
-      .map { |v| v.is_a?(Hash) ? v.to_a : [v, nil]}
-      .flatten
-      .map.with_index do |val, idx|
-        if idx % 2 == 0
-          name = val.to_s
-        else
-          records = instance_variable_get("@#{name}")
-          if records
-            ActiveModel::Serializer::CollectionSerializer.new(
-              records,
-              val
-            ).to_json
-          else
-            nil
-          end
-        end
-      end.compact
-    ]
+    args
+    .map { |v| v.is_a?(Hash) ? v.to_a : [v, nil]}
+    .flatten.each_slice(2)
+    .map do |(name, options)|
+      val = instance_variable_get("@#{name}")
+      [
+        name,
+        val ? ActiveModel::Serializer::CollectionSerializer.new(val, options).as_json : nil
+      ]
+    end.to_h
   end
 end
